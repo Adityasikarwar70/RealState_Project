0febac533418582d0b06e79011439254560bbd2c
ADITYA SINGH SIKARWAR <bs151439@gmail.com>
2024-03-23 23:23:35 +0530

    created listing API route

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import  Express  from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import authRouter from './routes/auth.route.js'
+import listingRouter from './routes/listing.route.js'
 import cookieParser from "cookie-parser";
 import dotenv from 'dotenv'
 dotenv.config();
@@ -24,6 +25,7 @@ app.listen(3000,()=>{
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 
 // middleware a function to deal with error or error handling
